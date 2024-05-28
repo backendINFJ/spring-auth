998fbefaeba41dcaf4b270e41be1e7f8260e2e98
yoonjae <backendINFJ@github.com>
2024-05-28 23:41:03 +0900

    Spring 3주차 - Spring Security 로그인 구현

---
+++ src/main/java/com/sparta/springauth/controller/ProductController.java
@@ -0,0 +1,26 @@
+package com.sparta.springauth.controller;
+
+
+import com.sparta.springauth.entity.User;
+import com.sparta.springauth.security.UserDetailsImpl;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api")
+public class ProductController {
+
+    @GetMapping("/products")
+    public String getProducts(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+        User user = userDetails.getUser();
+        System.out.println("user.getUsername() = " + user.getUsername());
+
+
+        return  "redirect:/";
+    }
+}
